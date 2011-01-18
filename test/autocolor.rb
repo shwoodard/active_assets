42a6e28c04aa93ea815386a19c069fb212eafb25
Sam Woodard <sam.h.woodard@gmail.com>
2011-01-17 17:35:04 -0800

    move autocolor

---
+++ test/autocolor.rb
@@ -0,0 +1,3 @@
+Test::Unit::AutoRunner.setup_option do |auto_runner, opts|
+  auto_runner.runner_options[:use_color] = true
+end
--- test/helper.rb
+++ test/helper.rb
@@ -11,7 +11,3 @@ Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| load f }
 include RailsHelper
 
 load File.join(rails_root, 'config/application.rb')
-
-Test::Unit::AutoRunner.setup_option do |auto_runner, opts|
-  auto_runner.runner_options[:use_color] = true
-end
