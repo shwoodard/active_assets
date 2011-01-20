14b7794dc70536120f032710241445e5a3d87d68
Sam Woodard <sam.h.woodard@gmail.com>
2011-01-20 10:52:49 -0800

    change example file

--- test/fixtures/rails_root/config/sprites.rb
+++ test/fixtures/rails_root/config/sprites.rb
@@ -1,5 +1,5 @@
 Rails.application.sprites do
-  sprite :sprite3 do
+  sprite 'sprites/3.png' => 'sprites/3.css' do
     _"sprite_images/sprite3/1.png" => ".klass_1"
     _"sprite_images/sprite3/2.png" => ".klass_2"
   end
