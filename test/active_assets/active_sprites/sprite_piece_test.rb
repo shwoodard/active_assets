b5cb3b67a2dca681e91d3ddb5698d039d812e204
Sam Woodard <sam.h.woodard@gmail.com>
2011-01-19 20:05:00 -0800

    some more assertions

--- test/active_assets/active_sprites/sprite_piece_test.rb
+++ test/active_assets/active_sprites/sprite_piece_test.rb
@@ -21,17 +21,25 @@ class SpritePiecetest < Test::Unit::TestCase
       end
     end
 
+    assert_equal "240px", sprites[:sprite1]["sprite_images/sprite1/1.png"].height
+    assert_equal "320px", sprites[:sprite1]["sprite_images/sprite1/1.png"].width
+    assert_equal "top", sprites[:sprite1]["sprite_images/sprite1/1.png"].y
     assert_equal 'right', sprites[:sprite1]["sprite_images/sprite1/1.png"].x
   end
 
   def test_configure_with_options
     Rails.application.sprites do
       sprite :sprite2 do
-        _"sprite_images/sprite1/1.png" => ".klass_1", :x => '0', :y => '320px'
+        _"sprite_images/sprite1/1.png" => ".klass_1", :x => 'right', :y => 'top', :width => '320px', :height => '240px'
         _"sprite_images/sprite1/2.png" => ".klass_2"
       end
     end
 
-    assert_equal '0', sprites[:sprite2]["sprite_images/sprite1/1.png"].x
+    assert_equal '.klass_2', sprites[:sprite2]["sprite_images/sprite1/2.png"].css_selector
+    assert_equal '.klass_1', sprites[:sprite2]["sprite_images/sprite1/1.png"].css_selector
+    assert_equal "240px", sprites[:sprite2]["sprite_images/sprite1/1.png"].height
+    assert_equal "320px", sprites[:sprite2]["sprite_images/sprite1/1.png"].width
+    assert_equal "top", sprites[:sprite2]["sprite_images/sprite1/1.png"].y
+    assert_equal 'right', sprites[:sprite2]["sprite_images/sprite1/1.png"].x
   end
 end
