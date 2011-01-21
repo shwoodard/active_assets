cdfec156a98740b885e1ccb4256f0a2a9c28f449
Sam Woodard <sam.h.woodard@gmail.com>
2011-01-21 14:51:37 -0800

    fix for paths with spaces

--- test/fixtures/rails_root/config/sprites.rb
+++ test/fixtures/rails_root/config/sprites.rb
@@ -7,7 +7,7 @@ Rails.application.sprites do
   sprite 'sprites/4.png' do
     Dir[Rails.root.join('public/images/sprite_images/sprite4/*.{png,gif,jpg}')].each do |path|
       image_path = path[%r{^.*/public/images/(.*)$}, 1]
-      klass_name = ".klass_#{File.basename(image_path, File.extname(image_path))}"
+      klass_name = ".klass_#{File.basename(image_path, File.extname(image_path)).split(' ').join('_')}"
       sp image_path => klass_name
     end
   end
