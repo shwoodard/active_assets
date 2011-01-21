0c8d4bae86f2c00876597858d58224d0598c54d2
Sam Woodard <sam.h.woodard@gmail.com>
2011-01-21 10:21:08 -0800

    cleanup when we're done

--- test/active_assets/active_sprites/runner_test.rb
+++ test/active_assets/active_sprites/runner_test.rb
@@ -7,10 +7,15 @@ class RunnerTest < Test::Unit::TestCase
   end
 
   def teardown
+    FileUtils.rm_rf(Rails.root.join('public/images/sprites'))
+    FileUtils.rm_rf(Rails.root.join('public/stylesheets/sprites'))
   end
 
-  def test_generate!
+  def test_generate
     assert_false Rails.application.sprites.all.empty?
+  end
+
+  def test_generate2
     assert_nothing_raised do
       Rails.application.sprites.generate!
     end
