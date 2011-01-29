115afffac1363006d7d8cde3395ff2091b51987f
Sam Woodard <sam.h.woodard@gmail.com>
2011-01-29 07:18:02 -0800

    use default

--- test/fixtures/rails_root/config/application.rb
+++ test/fixtures/rails_root/config/application.rb
@@ -7,5 +7,5 @@ module ActiveAssetsTest
   end
 end
 
-Rails.application.config.active_sprites.sprite_backend = :chunky_png
+# Rails.application.config.active_sprites.sprite_backend = :mini_magick
 Rails.application.config.active_support.deprecation = :stderr
