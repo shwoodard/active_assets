41f7d86c6cdb84341a06dbea4684c918a773f6f0
Sam Woodard <sam.h.woodard@gmail.com>
2011-02-22 15:45:05 -0800

    refactor

--- lib/active_assets/active_sprites.rb
+++ lib/active_assets/active_sprites.rb
@@ -15,22 +15,28 @@ module ActiveAssets
     def self.define_tasks
       desc "Generate sprites."
       task :sprites => :environment do
-        require 'active_assets'
-
-        Rails.application = Class.new(Rails::Application)
-        Rails.application.extend Rails::ActiveSprites
-
-        sprite_path = 'config/sprites'
-
-        if File.exists?(sprite_path)
-          require sprite_path
-        else
-          abort "No sprite configuration file at #{sprite_path}"
-        end
-
         ENV['VERBOSE'] ||= 'true'
         Rails.application.sprites.generate!
       end
+    end
+
+    def self.define_gem_tasks
+      require 'active_assets'
+      require 'rails/active_sprites'
+
+      Rails.application ||= Class.new(Rails::Application)
+      Rails.application.extend Rails::ActiveSprites
+
+      sprite_path = 'config/sprites'
+
+      if File.exists?(sprite_path)
+        require sprite_path
+      else
+        abort "No sprite configuration file at #{sprite_path}"
+      end
+
+      ENV['VERBOSE'] ||= 'true'
+      Rails.application.sprites.generate!
     end
 
   end
