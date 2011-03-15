19d119aaf802703c2af0180d41e9c9941169c38a
Sam Woodard <sam.h.woodard@gmail.com>
2011-03-15 08:32:23 -0700

    match on :// rather than https? so that user can specify a path and let the browser add the protocol

--- lib/active_assets/active_expansions/assets.rb
+++ lib/active_assets/active_expansions/assets.rb
@@ -85,7 +85,7 @@ module ActiveAssets
 
         def cleanse_paths!(paths)
           paths.map! do |path|
-            if path =~ %r{^https?}
+            if path =~ %r{://}
               path
             else
               dirname = File.dirname(path)
